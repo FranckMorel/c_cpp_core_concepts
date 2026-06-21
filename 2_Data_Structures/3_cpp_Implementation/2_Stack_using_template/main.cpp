6c4ce7e9c3fbc0a2f5d7a3cc941daddb94f4c90f
FranckMorel <franckmoreltonfack@gmail.com>
2026-06-21 20:29:38 +0200

    feat(cpp): add template stack with custom SensorData type

--- 2_Data_Structures/3_cpp_Implementation/2_Stack_using_template/main.cpp
+++ 2_Data_Structures/3_cpp_Implementation/2_Stack_using_template/main.cpp
@@ -1,7 +1,39 @@
 #include "stack_template.hpp"
 #include <iostream>
 
+
+struct sensorData
+{
+    int sensorId;
+    float value;
+    unsigned long timestamp;
+};
+
+
 int main()
 {
+    Stack<sensorData> sensorDataStack;
+    sensorData s1;
+
+    for (int i = 0; i < 5; i++)
+    {
+        s1.sensorId = i * 2;
+        s1.value = (i + 0.345f);
+        s1.timestamp = (i + 247637);
+
+        sensorDataStack.push(s1);
+    }
+
+    int i = 5;
+
+    while(sensorDataStack.pop(s1))
+    {
+        std::cout << "Sensor " << i << " : "
+                                     "Id: "     << s1.sensorId <<
+                               " | value: "     << s1.value <<
+                               " | timestamp: " << s1.timestamp <<
+                               "\n";
+        i--;
+    }
 
 }
