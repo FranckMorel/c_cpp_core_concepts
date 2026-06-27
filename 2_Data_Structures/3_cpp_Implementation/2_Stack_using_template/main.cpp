fe8f2c2fc8e1ad2234900f194393264c4b5f99ce
FranckMorel <franckmoreltonfack@gmail.com>
2026-06-27 23:51:01 +0200

    feat(cpp): compare explicit and implicit stack implementations

--- 2_Data_Structures/3_cpp_Implementation/2_Stack_using_template/main.cpp
+++ 2_Data_Structures/3_cpp_Implementation/2_Stack_using_template/main.cpp
@@ -1,6 +1,7 @@
 #include "stack_template.hpp"
+#include <cstring>
 #include <iostream>
-#include <string>
+
 
 using namespace std;
 
@@ -29,30 +30,106 @@ struct SensorData
 * 3. Due to the LIFO principle, the string
 * is reconstructed in reverse order.
   */
-string ReverseString(string s)
+void ReverseString(char* c, size_t len)
 {
     Stack<char> charStack;
 
-
     /* Push every character onto the stack */
-
-
-    for (size_t i = 0; i < s.length(); i++)
+    for (size_t i = 0; i < len; i++)
     {
-        charStack.push(s[i]);
+        charStack.push(c[i]);
     }
 
     /* Pop characters back into the string */
     size_t i = 0;
 
-    while (charStack.pop(s[i]))
+    while (charStack.pop(c[i]))
     {
         i++;
     }
 
-    return s;
-
 }
+
+/*
+ * Reverse a string using recursion.
+ *
+ * Unlike the previous implementation,
+ * this algorithm does not use an explicit
+ * stack data structure.
+ *
+ * Instead, recursion uses the implicit
+ * function call stack managed by the compiler
+ * and CPU.
+ */
+
+ void swap(char *left, char *right)
+{
+    /* Exchange the characters */
+    char temp = *left;
+    *left = *right;
+    *right = temp;
+}
+
+/*
+ * Recursive helper function.
+ *
+ * Algorithm:
+ * 1. Swap the left and right characters.
+ * 2. Move both pointers toward the center.
+ * 3. Repeat until both pointers meet or cross.
+ */
+void ReverseRecursive(char *left, char *right)
+{
+    /*
+     * Base Case
+     *
+     * Stop recursion when the middle
+     * of the string has been reached.
+     *
+     * left == right
+     *     Odd number of characters
+     *
+     * left > right
+     *     Even number of characters
+     */
+    if (left >= right)
+    {
+        return;
+    }
+
+    swap(left, right);
+
+    /*
+    * Recursive Call
+    *
+    * Solve a smaller subproblem by
+    * moving both pointers toward
+    * the center of the string.
+    */
+    ReverseRecursive(left + 1, right - 1);
+}
+
+/*
+ * Wrapper Function
+ *
+ * Calculates the string boundaries and
+ * starts the recursive algorithm.
+ */
+void ReverseStringRecursive(char *str)
+{
+    size_t len = strlen(str);
+
+    if (len == 0)
+    {
+        return;
+    }
+
+    char *left = &str[0];
+    char *right = &str[len - 1];
+
+    ReverseRecursive(left, right);
+}
+
 
 int main()
 {
@@ -92,12 +169,28 @@ int main()
  * Reverse a string using Stack<char>
  *************************************************/
 
-    string input;
+    char input[50];
 
-    cout << "Enter a String:\n";
-    getline(cin, input);
+    cout << "Reverse using Stack\n";
+    cout << "Enter a String: ";
+    cin.getline(input,50);
 
-    input = ReverseString(input);
+    ReverseString(input,strlen(input));
+
+    cout << "Reversed String: " << input << "\n";
+    cout << "\n\n";
+
+
+/*************************************************
+ * Example 3:
+ * Reverse a string using recursion
+*************************************************/
+
+    cout << "Reverse by Recursion\n";
+    cout << "Enter a String: ";
+    cin.getline(input, 50);
+
+    ReverseStringRecursive(input);
 
     cout << "Reversed String: " << input << "\n";
 
